21dc24ef462fce0b487e1c306017aab1515226c0
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-07 00:08:42 -0300

    feat:setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import { router } from './routes/routes';
 
 const app = express();
-app.use(router);
 app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Servidor em funcionamento");
