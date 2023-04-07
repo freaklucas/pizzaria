abb6204089d3eadbad704a401ebc39cd9499c7df
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-07 17:07:43 -0300

    feat:tratando erros

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,7 +2,7 @@ import {Router, Request, Response } from 'express';
 
 const router = Router();
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ ok: true})
+    return res.json({ name: "Lucas Oliveira"});
 })
 
 export {router};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,24 @@
+import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import { router } from './routes/routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
 app.use(router);
+app.use(cors());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return res.status(400).json({
+            error: err.message,
+        })
+    }
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+});
 
 app.listen(3000, () => {
     console.log("Servidor em funcionamento");
