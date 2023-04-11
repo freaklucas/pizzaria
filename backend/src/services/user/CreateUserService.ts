f1931d9eac1c6347e47cab26dfe2bcab093bc888
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-10 22:37:43 -0300

    feat:criando usuario

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 interface UserRequest {
   name: string;
   email: string;
@@ -6,7 +7,31 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    return { name, email, password };
+    if (!email) {
+      throw new Error("Email incorrect!");
+    }
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    });
+    return user;
   }
 }
 
