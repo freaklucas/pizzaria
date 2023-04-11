e426d38c10855767bf42c6558cd49ce4cf2fe340
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-10 23:38:03 -0300

    feat:bcrypt + hash senha

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
 interface UserRequest {
   name: string;
   email: string;
@@ -19,11 +20,12 @@ class CreateUserService {
     if (userAlreadyExists) {
       throw new Error("User already exists!");
     }
+    const passwordHash = await hash(password, 8);
     const user = await prismaClient.user.create({
       data: {
         name: name,
         email: email,
-        password: password,
+        password: passwordHash,
       },
       select: {
         id: true,
