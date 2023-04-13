84d45709497cd522d9025688d6dfc44bfde0c598
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-13 16:14:04 -0300

    criando middleware

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,11 +2,15 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "../controllers/user/CreateUserController";
 import { AuthUserController } from "../controllers/user/AutUserController";
 import { DetailUserController } from "../controllers/DetailUserController";
+
+import {isAuthenticated} from '../middlewares/isAuthenticated';
+
 const router = Router();
+
 
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
-router.get('/me', new DetailUserController().handle);
+router.get('/me',isAuthenticated, new DetailUserController().handle);
 
 export { router };
