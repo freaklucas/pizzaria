d86c49b8a1eac2c17ae8d53a53722caca135e235
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-13 15:21:20 -0300

    feat: detail user

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "../controllers/user/CreateUserController";
 import { AuthUserController } from "../controllers/user/AutUserController";
-
+import { DetailUserController } from "../controllers/DetailUserController";
 const router = Router();
+
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
+router.get('/me', new DetailUserController().handle);
 
 export { router };
