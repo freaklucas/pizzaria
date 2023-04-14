89a82c3aa515bf8387d0786ee8ae3b49c0fb10bc
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-14 00:43:17 -0300

    feat:detalhes do usuario

--- backend/src/controllers/DetailUserController.ts
+++ backend/src/controllers/DetailUserController.ts
@@ -2,12 +2,13 @@ import { Request, Response } from "express";
 import { DetailUserService } from "../services/user/DetailUserService";
 
 class DetailUserController {
-    async handle(req: Request, res: Response) {
-        const detailUserService = new DetailUserService();
-        const user = await detailUserService.execute();
+  async handle(req: Request, res: Response) {
+    const user_id = req.user_id;
+    const detailUserService = new DetailUserService();
+    const user = await detailUserService.execute(user_id);
 
-        return res.json(user);
-    }
+    return res.json(user);
+  }
 }
 
-export {DetailUserController};
+export { DetailUserController };
--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -20,6 +20,8 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
             process.env.JWT_SECRET
         ) as Payload;
 
+        req.user_id = sub;
+        
         console.log(sub);
         return next();
     }
