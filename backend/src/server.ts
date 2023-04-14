748da9d439b53f5086b277318cb1fa76891fe8dc
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-14 18:49:56 -0300

    feat:diretorio p imagem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,14 @@ import "express-async-errors";
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes/routes";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
 app.use(express.json());
 app.use(router);
 app.use(cors());
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
