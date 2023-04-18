b07ce84fcd59a3461c08a9392a93621fb10f42d5
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-17 23:41:11 -0300

    bugfix: erro listagem detail

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,22 +2,38 @@ import { Router, Request, Response } from "express";
 import multer from "multer";
 import uploadConfig from "../config/multer";
 
-import { CreateUserController } from "../controllers/user/CreateUserController";
-import { AuthUserController } from "../controllers/user/AutUserController";
-import { DetailUserController } from "../controllers/DetailUserController";
+import { CreateUserController } 
+  from "../controllers/user/CreateUserController";
+import { AuthUserController } 
+  from "../controllers/user/AutUserController";
+import { DetailUserController } 
+  from "../controllers/DetailUserController";
 
-import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { isAuthenticated } 
+  from "../middlewares/isAuthenticated";
 
-import { CreateCategoryController } from "../controllers/category/CreateCategoryController";
-import { ListCategoryController } from "../controllers/category/ListCategoryController";
-import { CreateProductController } from "../controllers/product/CreateProductController";
-import { ListByCategoryController } from "../controllers/product/ListByCategoryController";
-import { CreateOrderController } from "../controllers/order/CreateOrderController";
-import { RemoveOrderController } from "../controllers/order/RemoveOrderController";
-import { AddItemController } from "../controllers/order/AddItemController";
-import { RemoveItemController } from "../controllers/order/RemoveItemController";
-import { SendOrderController } from "../controllers/order/SendOrderController";
-import { ListOrderController } from "../controllers/order/ListOrderController";
+import { CreateCategoryController } 
+  from "../controllers/category/CreateCategoryController";
+import { ListCategoryController } 
+  from "../controllers/category/ListCategoryController";
+import { CreateProductController } 
+  from "../controllers/product/CreateProductController";
+import { ListByCategoryController } 
+  from "../controllers/product/ListByCategoryController";
+import { CreateOrderController } 
+  from "../controllers/order/CreateOrderController";
+import { RemoveOrderController } 
+  from "../controllers/order/RemoveOrderController";
+import { AddItemController } 
+  from "../controllers/order/AddItemController";
+import { RemoveItemController } 
+  from "../controllers/order/RemoveItemController";
+import { SendOrderController } 
+  from "../controllers/order/SendOrderController";
+import { ListOrderController } 
+  from "../controllers/order/ListOrderController";
+import { DetailOrderController } 
+  from "../controllers/order/DetailOrderController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -46,12 +62,28 @@ router.post(
   new CreateProductController().handle
 );
 
-router.post("/order", isAuthenticated, new CreateOrderController().handle);
-router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
-router.post("/order/add", isAuthenticated, new AddItemController().handle);
-router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle);
-router.put("/order/send", isAuthenticated, new SendOrderController().handle);
-router.get("/orders", isAuthenticated, new ListOrderController().handle);
-
+router.post("/order", isAuthenticated, 
+  new CreateOrderController().handle
+);
+router.delete("/order", isAuthenticated, 
+  new RemoveOrderController().handle
+);
+router.post("/order/add", isAuthenticated, 
+  new AddItemController().handle
+);
+router.delete(
+  "/order/remove",
+  isAuthenticated,
+  new RemoveItemController().handle
+);
+router.put("/order/send", isAuthenticated, 
+  new SendOrderController().handle
+);
+router.get("/orders", isAuthenticated, 
+  new ListOrderController().handle
+);
+router.get("/order/detail", isAuthenticated, 
+  new DetailOrderController().handle
+);
 
 export { router };
