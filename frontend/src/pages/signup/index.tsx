c4ebb9d9aa1eaf38d559f28bbe87b68e173ce1b5
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-21 13:22:49 -0300

    feat:page signin

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,21 +1,26 @@
 import Head from "next/head";
-import pizza from "../../public/pizza.png";
-import styles from "../../styles/home.module.scss";
+import pizza from "../../../public/pizza.png";
+import styles from "../../../styles/home.module.scss";
 import Image from "next/image";
-import { Input } from "../components/ui/Input/index";
-import { Button } from "../components/ui/Button/index";
+import { Input } from "../../components/ui/Input/index";
+import { Button } from "../../components/ui/Button/index";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Pizzaria - Faça seu login</title>
+        <title>Faça seu cadastro!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={pizza} alt="logo pizza" width={520} height={300} />
         <div className={styles.login}>
+          <h1>Crie sua conta</h1>
           <form>
+          <Input 
+              placeholder="Insira seu nome" 
+              type="text" 
+            />
            <Input 
               placeholder="Insira seu Email" 
               type="text" 
@@ -28,12 +33,12 @@ export default function Home() {
               type="submit"
               loading={true}
             > 
-              Acessar
+              Criar conta
             </Button> 
           </form>
           <a className={styles.text}>
-            <Link href="/signup">
-              Não possui uma conta? Cadastre-se
+            <Link href="/">
+              Já possui uma conta? Faça login
             </Link>
           </a>
         </div>
