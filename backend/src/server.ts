1533f3dbcdeccfeacadcb563b39a520e427ca85c
freaklucas <lucas.oliveira.freak@gmail.com>
2023-04-22 18:21:33 -0300

    fix: cors ok

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
-import "express-async-errors";
 import express, { Request, Response, NextFunction } from "express";
-import { router } from "./routes/routes";
+import "express-async-errors";
 import cors from "cors";
 import path from "path";
 
-const app = express();
+import { router } from "./routes/routes";
 
+const app = express();
 app.use(express.json());
-app.use(router);
 app.use(cors());
+
+app.use(router);
+
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -17,12 +19,11 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
       error: err.message,
     });
   }
+
   return res.status(500).json({
     status: "error",
     message: "Internal server error.",
   });
 });
 
-app.listen(3333, () => {
-  console.log("Servidor em funcionamento");
-});
+app.listen(3333, () => console.log("Servidor rodando..."));
